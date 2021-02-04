38b8388ff1c61d213bd4f0e83882f48eba70d334
Vivianne Ramselaar <vmb318@nyu.edu>
2021-02-04 16:10:16 -0500

    Initial Commit

---
+++ Pre-work/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pre-work
+//
+//  Created by Vivianne Bollee on 2/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
